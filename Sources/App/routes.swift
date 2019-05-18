755fe915fd360681d90e34d44b05a9060624d93b
Arvid Kongstad <arvid.kongstad@gmail.com>
2019-05-18 19:19:52 +0100

    Add HTML form to view and POST route to verify password

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,10 +5,24 @@ import Vapor
 ///
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
 public func routes(_ router: Router) throws {
-    router.get("hello") { req in
-        return "Hello, world!"
-    }
+
     router.get { request in
         return try request.view().render("home")
     }
+
+    router.post { request in
+        return try request.content.decode(FormInput.self).map(to: String.self) { input in
+
+            if input.password.lowercased() == "strawberries" {
+                return "Well done! That is the correct password 🎉"
+            } else {
+                return "Sorry, that's not the correct password 🤡"
+            }
+        }
+    }
+
+}
+
+struct FormInput: Content {
+    let password: String
 }
