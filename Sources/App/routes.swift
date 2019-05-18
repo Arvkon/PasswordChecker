c3f42d9c9cc1d358e624f341e5196f18a5ef47a9
Arvid Kongstad <arvid.kongstad@gmail.com>
2019-05-18 14:26:54 +0100

    Add root route rendering new Leaf HTML template

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,7 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
+    router.get { request in
+        return try request.view().render("home")
+    }
 }
